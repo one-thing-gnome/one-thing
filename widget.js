23631d3a30c14a350091b64291932785bf0fc399
Dante Calderon <dantehemerson@gmail.com>
2023-01-15 01:30:23 -0500

    Allow better editing

--- widget.js
+++ widget.js
@@ -110,7 +110,9 @@ class _Widget extends PanelMenu.Button {
   _initContainer() {
     // container for entry and icon elements
     const calcBox = new St.BoxLayout();
+
     calcBox.add(this.panelText);
+
     this.add_actor(calcBox);
   }
 
@@ -125,10 +127,12 @@ class _Widget extends PanelMenu.Button {
       }.bind(this)
     );
 
-    this.inputText.clutter_text.connect(
-      "button-release-event",
-      this._onButtonReleaseEntry.bind(this)
-    );
+    // @Deprecated: Allow to select all text on focus
+    // Depreacating it makes the entry fields works as a normal entry field around the world
+    // this.inputText.clutter_text.connect(
+    //   "button-release-event",
+    //   this._onButtonReleaseEntry.bind(this)
+    // );
 
     this.inputText.clutter_text.connect(
       "activate",
@@ -136,14 +140,15 @@ class _Widget extends PanelMenu.Button {
     );
   }
 
-  _onButtonReleaseEntry(actor) {
-    if (
-      actor.get_cursor_position() !== -1 &&
-      actor.get_selection().length === 0
-    ) {
-      actor.set_selection(0, actor.get_text().length); // doesn't work to do this on button-press-event or key_focus_in; don't know why
-    }
-  }
+  // @Deprecated: Allow to select all text on focus
+  // _onButtonReleaseEntry(actor) {
+  //   if (
+  //     actor.get_cursor_position() !== -1 &&
+  //     actor.get_selection().length === 0
+  //   ) {
+  //     actor.set_selection(0, actor.get_text().length); // doesn't work to do this on button-press-event or key_focus_in; don't know why
+  //   }
+  // }
 
   _onActivateEntry(actor) {
     let result;
@@ -153,7 +158,13 @@ class _Widget extends PanelMenu.Button {
     } catch (e) {
       result = "Unexpected error";
     }
-    this.panelText.set_text(result);
+
+    log("Setting text " + result);
+
+    if (this.panelText !== "") {
+      this.panelText.set_text(result);
+    } else {
+    }
     this.menu.close();
   }
 }
